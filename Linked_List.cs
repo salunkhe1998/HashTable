8f33a4f3590089bdbd30fa1a885c0f01f71d35db
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 15:35:34 +0530

    Linked List Program

---
+++ Linked_List.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAssignment
+{
+    public class Linked_List<K, V> where K : IComparable
+    {
+        public Hash_Node<K, V> Head;
+        public Hash_Node<K, V> Tail;
+
+        public Linked_List()
+        {
+            Head = null;
+            Tail = null;
+        }
+        internal Hash_Node<K, V> Search(K Key)
+        {
+            Hash_Node<K, V> temp = Head;
+            while (temp != null)
+            {
+                if (temp.Key.Equals(Key))
+                {
+                    return temp;
+                }
+                temp = temp.Next;
+            }
+            return null;
+        }
+        public void Append(Hash_Node<K, V> node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+            }
+        }
+    }
+}
