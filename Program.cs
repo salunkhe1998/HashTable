c3c1a693888c498dde8fc40a912bb94a61950c33
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 15:25:42 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -12,8 +12,20 @@ namespace HashTableAssignment
         {
             public static void Main(string[] args)
             {
-                Console.WriteLine("***** Welcome to Hashtable Program *****");
+                Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
 
+                string Sentence = "to be or not to be";
+                string[] Words = Sentence.Split(" ");
+                foreach (string word in Words)
+                {
+                    int Value = linked_hash_map.Get(word);
+                    if (Value == default)
+                        Value = 1;
+                    else Value++;
+                    linked_hash_map.Add(word, Value);
+                }
+                int Frequency = linked_hash_map.Get("to");
+                Console.WriteLine("Frquency of word in mentioned statement is : " + Frequency);
             }
         }
     }
