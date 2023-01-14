56616b34ad8b57ff8fdbcf5523a08ffc12649541
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 15:46:34 +0530

    Ability to find frequency in a large paragraph

--- Program.cs
+++ Program.cs
@@ -6,27 +6,30 @@ using System.Threading.Tasks;
 
 namespace HashTableAssignment
 {
-    namespace HashTable
+    class Program
     {
-        class Program
+        public static void Main(string[] args)
         {
-            public static void Main(string[] args)
-            {
-                Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
+            Console.WriteLine("Welcome to Hashtable Program");
+            Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
 
-                string Sentence = "to be or not to be";
-                string[] Words = Sentence.Split(" ");
-                foreach (string word in Words)
-                {
-                    int Value = linked_hash_map.Get(word);
-                    if (Value == default)
-                        Value = 1;
-                    else Value++;
-                    linked_hash_map.Add(word, Value);
-                }
-                int Frequency = linked_hash_map.Get("to");
-                Console.WriteLine("Frquency of word in mentioned statement is : " + Frequency);
+            string Sentence = "paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] Words = Sentence.Split(" ");
+            foreach (string word in Words)
+            {
+                int Value = linked_hash_map.Get(word);
+                if (Value == default)
+                    Value = 1;
+                else Value++;
+                linked_hash_map.Add(word, Value);
             }
+            int Frequency = linked_hash_map.Get("paranoid");
+            Console.WriteLine("\"paranoid\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("are");
+            Console.WriteLine("\"are\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("they");
+            Console.WriteLine("\"they\" comes {0} times in the given paragraph", Frequency);
         }
     }
 
