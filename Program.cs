2745bafb28b849f1cd1c7529f3ab8c8866d33eaa
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 15:13:15 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAssignment
+{
+    namespace HashTable
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("***** Welcome to Hashtable Program *****");
+
+            }
+        }
+    }
+
+}
