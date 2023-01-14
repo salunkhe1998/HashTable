4f8ad0f546976e401f33f53caafa0f2a3a947a79
Ashwini <salunkheashwini1998@gmail.com>
2023-01-14 15:29:19 +0530

    node constructor

---
+++ Hash_Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAssignment
+{
+    public class Hash_Node<K, V>
+    {
+        public K Key;
+        public V Value;
+        public Hash_Node<K, V> Next;
+
+        public Hash_Node(K Key, V Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+            Next = null;
+        }
+    }
+}
